af31815e45ee0c6bc5dab574f4625bdccc974ec7
Marc Mezger <marc.mezger@gmail.com>
2023-04-14 21:05:42 +0200

    Adding Aleph Alpha Backend.

--- agent/backend.py/aleph_alpha_service.py
+++ agent/backend.py/aleph_alpha_service.py
@@ -1,54 +1,56 @@
 import os
 
+from dotenv import load_dotenv
 from langchain.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.embeddings import AlephAlphaAsymmetricSemanticEmbedding
-from langchain.vectorstores import Qdrant
+from langchain.vectorstores import Chroma
 from loguru import logger
 from omegaconf import DictConfig
-from qdrant_client import QdrantClient
 
 from agent.utils.configuration import load_config
 
-# loguru make file logger
-logger.add("logs/{time}.log")
+load_dotenv()
 
 
-@load_config(location="config/db.yml")
-def initialize_qdrant(cfg: DictConfig, aleph_alpha_token: str) -> QdrantClient:
-    embeddings = AlephAlphaAsymmetricSemanticEmbedding()
-    client = QdrantClient(cfg.db.url, port=cfg.db.port, prefer_grpc=True)
-    qdrant = Qdrant(client=client, collection_name="db", embedding_function=embeddings.embed_query)
+@load_config(location="config/chroma_db.yml")
+def get_db_connection_(cfg: DictConfig, aleph_alpha_token: str):
+    logger.info(aleph_alpha_token)
 
-    return qdrant
+    embedding = AlephAlphaAsymmetricSemanticEmbedding()
+    vector_db = Chroma(persist_directory=cfg.chroma.persist_directory_aa, embedding_function=embedding)
+
+    return vector_db
 
 
-def embedd_files(path_to_dir: str, aleph_alpha_token: str):
-    loader = DirectoryLoader(path_to_dir, glob="*.pdf", loader_cls=PyPDFLoader)
+@load_config(location="config/chroma_db.yml")
+def embedd_documents(cfg: DictConfig, dir: str, aleph_alpha_token: str):
+    vector_db = get_db_connection_(aleph_alpha_token=aleph_alpha_token)
+
+    loader = DirectoryLoader(dir, glob="*.pdf", loader_cls=PyPDFLoader)
     docs = loader.load()
 
     logger.info(f"Loaded {len(docs)} documents.")
+    texts = [doc.page_content for doc in docs]
+    metadatas = [doc.metadata for doc in docs]
+    vector_db.add_texts(texts=texts, metadatas=metadatas)
 
-    # qdrant = initialize_qdrant(aleph_alpha_token=aleph_alpha_token)
-
-    embeddings = AlephAlphaAsymmetricSemanticEmbedding()
-
-    doc_result = embeddings.embed_documents(docs)
-
-    print(doc_result)
+    vector_db.persist()
 
 
-# def retrieve_documents(query: str, token: str, store):
-#     chain = VectorDBQAWithSourcesChain.from_llm(
-#         llm=AlephAlpha(aleph_alpha_api_key=token), vectorstore=store
-#     )
-#     result = chain({"question": query})
+def search_documents(aleph_alpha_token: str, query: str):
+    vector_db = get_db_connection_(aleph_alpha_token)
 
-#     return result
+    docs = vector_db.similarity_search_with_score(query, k=3)
+
+    return docs
 
 
 if __name__ == "__main__":
+    embedd_documents("data", os.getenv("AA_Token"))
 
-    # write the token to the env variable
-    os.environ["ALEPH_ALPHA_API_KEY"] = ".."
+    os.environ["ALEPH_ALPHA_API_KEY"] = os.getenv("AA_Token")
 
-    embedd_files("data", "adsf")
+    print(os.getenv("ALEPH_ALPHA_API_KEY"))
+
+    DOCS = search_documents(aleph_alpha_token=os.getenv("AA_token"), query="Muss ich mein Mietwagen volltanken?")
+    print(DOCS)
--- agent/backend.py/open_ai_service.py
+++ agent/backend.py/open_ai_service.py
@@ -1,33 +1,54 @@
+"""This script is used to initialize the chroma db backend."""
+import os
+
+from dotenv import load_dotenv
+from langchain.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Qdrant
+from langchain.vectorstores import Chroma
 from loguru import logger
 from omegaconf import DictConfig
-from qdrant_client import QdrantClient
 
 from agent.utils.configuration import load_config
 
-# loguru make file logger
-logger.add("logs/{time}.log")
+load_dotenv()
 
 
-@load_config(location="config/db.yml")
-def initialize_db_connection(cfg: DictConfig, open_ai_token: str) -> QdrantClient:
-    embeddings = OpenAIEmbeddings(openai_api_key=open_ai_token)
-    client = QdrantClient(cfg.db.url, port=cfg.db.port, prefer_grpc=True)
-    qdrant = Qdrant(client=client, collection_name="open_ai_db", embedding_function=embeddings.embed_query)
+@load_config(location="config/chroma_db.yml")
+def get_db_connection_(cfg: DictConfig, open_ai_token: str):
 
-    return qdrant
+    embedding = OpenAIEmbeddings(chunk_size=1, openai_api_key=open_ai_token)
+    vector_db = Chroma(persist_directory=cfg.chroma.persist_directory, embedding_function=embedding)
 
-
-def embedd_new_documents():
-    pass
+    return vector_db
 
 
-def search_db():
-    pass
+@load_config(location="config/chroma_db.yml")
+def embedd_documents(cfg: DictConfig, dir: str, open_ai_token: str):
+    vector_db = get_db_connection_(open_ai_token=open_ai_token)
+
+    loader = DirectoryLoader(dir, glob="*.pdf", loader_cls=PyPDFLoader)
+    docs = loader.load()
+
+    logger.info(f"Loaded {len(docs)} documents.")
+    texts = [doc.page_content for doc in docs]
+    metadatas = [doc.metadata for doc in docs]
+    vector_db.add_texts(texts=texts, metadatas=metadatas)
+
+    vector_db.persist()
+
+
+def search_documents(open_ai_token: str, query: str):
+    vector_db = get_db_connection_(open_ai_token=open_ai_token)
+
+    docs = vector_db.similarity_search_with_score(query, k=3)
+
+    return docs
 
 
 if __name__ == "__main__":
-    initialize_db_connection(open_ai_token="asdf")
-    # embedd_new_documents()
-    # search_db()
+    os.environ["OPENAI_API_TYPE"] = "azure"
+    os.environ["OPENAI_API_BASE"] = "https://openaiendpoint.openai.azure.com/"
+    embedd_documents("data", os.getenv("OPENAI_API_KEY"))
+
+    DOCS = search_documents(open_ai_token="", query="Muss ich mein Mietwagen volltanken?")
+    print(DOCS)
