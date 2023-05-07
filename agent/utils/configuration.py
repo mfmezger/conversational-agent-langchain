519e57ef8dd016cdce2ac4a6b7d64012a49f8f8c
Marc Mezger <marc.mezger@gmail.com>
2023-05-07 15:22:12 +0200

    Refactoring.

--- agent/utils/configuration.py
+++ agent/utils/configuration.py
@@ -1,9 +1,17 @@
+"""Configuration Wrapper."""
 from omegaconf import OmegaConf
 
 
 def load_config(location):
+    """Method to load the configuration file.
+
+    :param location: _description_
+    :type location: _type_
+    """
     def decorator(func):
+        """Decorator to load the configuration file."""
         def wrapper(*args, **kwargs):
+            """Wrapper to load the configuration file."""
             # Load the config file
             cfg = OmegaConf.load(location)
             return func(cfg, *args, **kwargs)
