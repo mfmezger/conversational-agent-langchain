5ebc127051fe4f7c4f3530d4ce7231fd62fc0b86
Marc Fabian Mezger <57255687+mfmezger@users.noreply.github.com>
2023-06-28 17:25:53 +0200

    Adding documentation messages. (#24)

--- agent/backend/aleph_alpha_service.py
+++ agent/backend/aleph_alpha_service.py
@@ -3,7 +3,7 @@ import os
 from typing import Any, Dict, List, Tuple, Union
 
 import numpy as np
-from aleph_alpha_client import (
+from aleph_alpha_client import (  # type: ignore
     Client,
     CompletionRequest,
     Document,
