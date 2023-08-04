275e267779aff81248004279d55758261bb67e9d
Marc Mezger <marc.mezger@gmail.com>
2023-08-04 15:25:30 +0200

    renaming of the rest services.

--- agent/api.py
+++ agent/api.py
@@ -118,7 +118,7 @@ def embedd_documents_wrapper(folder_name: str, llm_backend: str = "openai", toke
         raise ValueError("Please provide either 'aleph-alpha' or 'openai' as a parameter. Other backends are not implemented yet.")
 
 
-@app.post("/embedd_documents")
+@app.post("/embeddings/documents")
 async def post_embedd_documents(files: List[UploadFile] = File(...), llm_backend: str = "openai", token: Optional[str] = None) -> JSONResponse:
     """Uploads multiple documents to the backend.
 
@@ -152,7 +152,7 @@ async def post_embedd_documents(files: List[UploadFile] = File(...), llm_backend
     return JSONResponse(content={"message": "Files received and saved.", "filenames": file_names})
 
 
-@app.post("/embedd_document/")
+@app.post("/embeddings/document/")
 async def post_embedd_document(file: UploadFile, llm_backend: str = "openai", token: Optional[str] = None) -> JSONResponse:
     """Uploads one document to the backend and embeds it in the database.
 
@@ -183,7 +183,7 @@ async def post_embedd_document(file: UploadFile, llm_backend: str = "openai", to
     return JSONResponse(content={"message": "File received and saved.", "filenames": file.filename})
 
 
-@app.post("/embedd_text/")
+@app.post("/embeddings/text/")
 async def embedd_text(request: EmbeddTextRequest) -> JSONResponse:
     """Embeds text in the database.
 
@@ -213,7 +213,7 @@ async def embedd_text(request: EmbeddTextRequest) -> JSONResponse:
         raise ValueError("Please provide either 'aleph-alpha', 'gpt4all' or 'openai' as a parameter. Other backends are not implemented yet.")
 
 
-@app.post("/embedd_text_file/")
+@app.post("/embeddings/texts/files")
 async def embedd_text_files(request: EmbeddTextFilesRequest) -> JSONResponse:
     """Embeds text files in the database.
 
@@ -254,7 +254,7 @@ async def embedd_text_files(request: EmbeddTextFilesRequest) -> JSONResponse:
     return JSONResponse(content={"message": "Files received and saved.", "filenames": file_names})
 
 
-@app.post("/search")
+@app.post("/semantic/search")
 def search(request: SearchRequest) -> JSONResponse:
     """Searches for a query in the vector database.
 
@@ -353,7 +353,7 @@ def question_answer(request: QARequest) -> JSONResponse:
     return JSONResponse(content={"answer": answer, "prompt": prompt, "meta_data": meta_data})
 
 
-@app.get("/explain-qa")
+@app.get("/explanation/explain-qa")
 def explain_question_answer(query: Optional[str] = None, llm_backend: str = "openai", token: Optional[str] = None, amount: int = 1) -> JSONResponse:
     """Answer a question & explains it based on the documents in the database.
 
@@ -384,7 +384,7 @@ def explain_question_answer(query: Optional[str] = None, llm_backend: str = "ope
     return JSONResponse(content={"answer": answer, "prompt": prompt, "meta_data": meta_data})
 
 
-@app.post("/explain")
+@app.post("/explaination/aleph_alpha_explain")
 def explain_output(request: ExplainRequest) -> JSONResponse:
     """Explain the output of the question answering system.
 
