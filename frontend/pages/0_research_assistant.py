e10b73df0f9d04c765390d6300ff0b94a45d8a23
Marc Mezger <marc.mezger@gmail.com>
2023-09-15 13:43:28 +0200

    wip.

---
+++ frontend/pages/0_research_assistant.py
@@ -0,0 +1,63 @@
+"""The main gui."""
+from pathlib import Path
+
+import requests
+import streamlit as st
+from loguru import logger
+
+# Constants
+PDF_FILE_TYPE = "pdf"
+META_DATA_HEIGHT = 500
+EXPLANATION_HEIGHT = 300
+
+url = "http://localhost:8001/semantic/search"
+
+
+logger.info("Starting Application.")
+
+# Set small icon in the tab bar
+st.set_page_config(page_title="Information Retrieval Embedding Demo", page_icon=":mag:", layout="wide")
+
+# Create title
+st.title("Conversational AI")
+
+
+def create_folder_structure(folder_path: str) -> None:
+    """Create the folder structure."""
+    Path(folder_path).mkdir(parents=True, exist_ok=True)
+
+
+def initialize() -> None:
+    """Initialize the GUI."""
+    answer = ""
+    prompt = ""
+
+    # The user needs to enter the aleph alpha api key
+    aleph_alpha_api_key = st.text_input("Aleph Alpha Token", type="password")
+
+    st.session_state.api_key = aleph_alpha_api_key
+    logger.debug("API Key was entered")
+
+    # Search the documents
+    search_query = st.text_input("Search Query")
+    if st.button("Start Search", key="start_search") and search_query:
+        logger.debug("Search was started")
+
+        # Search the documents
+        documents = requests.post(
+            url,
+            json={
+                "query": search_query,
+                "llm_backend": "aa",
+                "token": st.session_state.api_key,
+                "amount": 5,
+            },
+        ).json()
+
+        st.write(documents)
+
+
+# Start the GUI app
+initialize()
+
+st.markdown("If you encounter any problems please contact us at: marc.mezger@adesso.de")
