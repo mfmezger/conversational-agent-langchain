481b86ff510ba6421357c7edaba3410244d0e130
Marc Mezger <marc.mezger@gmail.com>
2024-02-06 09:30:47 +0100

    updating.

---
+++ agent/backend/LLMBase.py
@@ -0,0 +1,39 @@
+"""Strategy Pattern."""
+from abc import ABC, abstractmethod
+
+from agent.data_model.request_data_model import LLMProvider, QARequest, SearchRequest
+
+
+class LLMBase(ABC):
+    """The LLM Base Strategy."""
+
+    @abstractmethod
+    def __init__(self, token: str, collection_name: str):
+        """Init the LLM Base."""
+        self.token = token
+        self.collection_name = collection_name
+
+    @abstractmethod
+    def embed_documents(self, dir: str, collection_name: Optional[str] = None):
+        """Embedd new docments in the Qdrant DB."""
+        pass
+
+    @abstractmethod
+    def create_collection(self, llm_provider: LLMProvider):
+        """Create a new collection in the Vector Database."""
+        pass
+
+    @abstractmethod
+    def search(self, search_request: SearchRequest):
+        """Searches the documents in the Qdrant DB with semantic search."""
+        pass
+
+    @abstractmethod
+    def rag(self, qa_request: QARequest):
+        """Retrieval Augmented Generation."""
+        pass
+
+    @abstractmethod
+    def summarize_text(self, text: str) -> str:
+        """Summarize text."""
+        pass
